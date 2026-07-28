33f7d88fe9170c1f135973065e4da355beeba6bb
DANISH Sidiq <187429155+Daniishhhhh@users.noreply.github.com>
2026-07-29 01:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (72.43%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -10,23 +10,19 @@ class Solution:
         res: List[int] = []
 
         while top <= bottom and left <= right:
-            # traverse from left to right along the top row
             for c in range(left, right + 1):
                 res.append(matrix[top][c])
             top += 1
 
-            # traverse from top to bottom along the right column
             for r in range(top, bottom + 1):
                 res.append(matrix[r][right])
             right -= 1
 
-            # traverse from right to left along the bottom row (if still valid)
             if top <= bottom:
                 for c in range(right, left - 1, -1):
                     res.append(matrix[bottom][c])
                 bottom -= 1
 
-            # traverse from bottom to top along the left column (if still valid)
             if left <= right:
                 for r in range(bottom, top - 1, -1):
                     res.append(matrix[r][left])
