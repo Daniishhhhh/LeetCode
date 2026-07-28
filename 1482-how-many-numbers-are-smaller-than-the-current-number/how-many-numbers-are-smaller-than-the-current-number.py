cc71d0ee82d60af9a70799e62afc3c4d1854489f
DANISH Sidiq <187429155+Daniishhhhh@users.noreply.github.com>
2026-07-29 00:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (21.70%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,18 +1,14 @@
+from typing import List
+
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        temp = sorted(nums)
-        
-        d={}
-        for i,num in enumerate(temp):
-            if num not in d:
-                d[num] =i
+        # Works when nums values are in the range 0..100
+        count = [0] * 102  # 0..101, we store frequency at index num+1
 
-        ret =[]     
+        for num in nums:
+            count[num + 1] += 1
 
-        for i in nums:
-            ret.append(d[i])   
+        for i in range(1, 102):
+            count[i] += count[i - 1]
 
-        return ret
-
-
-        
+        return [count[num] for num in nums]
