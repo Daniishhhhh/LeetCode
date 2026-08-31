68cb2760e29fc25e6dd74918c0f21c646e5d7d8a
DANISH Sidiq <187429155+Daniishhhhh@users.noreply.github.com>
2026-08-31 11:04:47 +0530

    Time: 5 ms (62.51%) | Memory: 44.1 MB (95.10%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) 
+    return false;   // quick reject
+
+    int[] count = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+        count[s.charAt(i) - 'a']++;   // increment for s
+        count[t.charAt(i) - 'a']--;   // decrement for t
+    }
+    for (int c : count) 
+    if (c != 0) return false; // any leftover means mismatch
+    return true;
+}  
+    }
+
