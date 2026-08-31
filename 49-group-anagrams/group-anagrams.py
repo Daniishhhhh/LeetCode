5ac05f643c8e3215babdeff0dbaa04b47249a052
DANISH Sidiq <187429155+Daniishhhhh@users.noreply.github.com>
2026-08-31 12:06:39 +0530

    Time: 11 ms (82.70%) | Memory: 22.2 MB (53.12%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for w in strs:
+            key = ''.join(sorted(w))
+            groups[key].append(w)
+        return list(groups.values())
